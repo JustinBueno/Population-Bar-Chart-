2b38af35d219e2b116af09af397133d19b70e793
JustinBueno <justin.mirabueno97@gmail.com>
2017-07-05 20:03:35 -0700

    Update population.cpp

--- population.cpp
+++ population.cpp
@@ -4,29 +4,28 @@ using namespace std;
 
 int main ()
 {
-    cout << "PRAIRIEVILLE POPULATION";
-    cout << "(each * represents 1000 people)";
+    cout << "PRAIRIEVILLE POPULATION" << endl;
+    cout << "(each * represents 1000 people)" << endl;
 
     ifstream inputfile;
     inputfile.open("People.txt");
-    
-    if (!inputfile)
-    { cout << "Error failure to open file" << endl;
-    }
-    
+
+   // if (!inputfile)
+   // { cout << "Error failure to open file" << endl;
+   // }
+
     for (int year = 1900; year <= 2000; year += 20)
     {
-        cout << year;
+        cout << endl << year << ' ';
         int pop;
         inputfile >> pop;
         for (int i = 1; i <= pop / 1000; i++)
-        {
-            cout << '*';
+        {   cout << '*';
+
         }
-        
-      
     }
-    inputfile.close();
+inputfile.close(); //Close the file
+
     return 0;
-    
 }
+
